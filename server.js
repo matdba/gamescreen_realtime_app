bee9effc079197c26893afc799b6f3358cc0e8f2
Martin <matingrimes@yahoo.com>
2019-04-09 14:06:08 +0430

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+var app = express();
+var port = process.env.PORT || 3000;
+var server = http.createServer(app);
+var io = socketIO(server);
+
+app.get('/', (req, res) => {
+  res.send("blah");
+});
+
+io.on('connection', (socket) => {
+  console.log('user is connected');
+
+  socket.on('join', () => {
+    socket.emit('newUserJoined', "this is from the server");
+  });
+
+  // socket.on('moveAction', () => {
+  //
+  // });
+
+});
+
+server.listen(port, () => {
+  console.log('Server is running on port 3000');
+});
